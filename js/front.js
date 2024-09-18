1ef5c50ea562c1d5f61b86c6bdc3536e7823fc5a
12-Click <info12click@icloud.com>
2024-09-18 12:54:30 -0600

    Update front.js

--- js/front.js
+++ js/front.js
@@ -38,34 +38,23 @@ document.addEventListener('DOMContentLoaded', function () {
 		HERO SLIDER
 	===================================================== */
 	var heroSlider = new Swiper('.hero-slider', {
-  slidesPerView: 1,
-  spaceBetween: 0,
-  loop: true,
-  effect: 'fade',
-  autoplay: {
-    delay: 4000,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    // Add responsive breakpoints to control Swiper behavior
-    768: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    1024: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    }
-  }
-});
+		slidesPerView: 1,
+		spaceBetween: 0,
+		loop: true,
+		effect: 'fade',
+		autoplay: {
+			delay: 4000,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
 
 	/* =====================================================
 		SCROLL TOP BUTTON [SHOW & HIDE & CLICKING]
